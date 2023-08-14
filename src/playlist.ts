1abb3c90322e2923c1c6ab8caab60200d89cfdbc
Cafe137 <xcafe137@gmail.com>
2023-08-14 17:46:33 +0200

    feat: create live stream

--- src/playlist.ts
+++ src/playlist.ts
@@ -1,11 +1,11 @@
 export function createPlaylist(segments: string[]) {
     const playlist = [
         '#EXTM3U',
+        '#EXT-X-PLAYLIST-TYPE:EVENT',
         '#EXT-X-VERSION:3',
         '#EXT-X-TARGETDURATION:10',
         '#EXT-X-MEDIA-SEQUENCE:0',
-        ...segments.map((segment, index) => `#EXTINF:10.0,\n${segment}`),
-        '#EXT-X-ENDLIST'
+        ...segments.map((segment, index) => `#EXTINF:10.0,\nhttp://localhost:1633/bzz/${segment}/`)
     ].join('\n')
     return playlist
 }
--- src/uploader.ts
+++ src/uploader.ts
@@ -1,15 +1,21 @@
 import { Bee } from '@ethersphere/bee-js'
 import { Arrays, System } from 'cafe-utility'
+import { Wallet } from 'ethers'
 import { readFile, readdir, unlink } from 'fs/promises'
 import { createPlaylist } from './playlist'
 
 export async function startUploader() {
     const bee = new Bee(Arrays.requireStringArgument(process.argv, 'bee'))
     const stamp = Arrays.requireStringArgument(process.argv, 'stamp')
+    const feedStamp = Arrays.requireStringArgument(process.argv, 'feedStamp')
 
     const processedSegments: Record<string, string> = {}
 
-    const feed = bee.makeFeedWriter('sequence', '00'.repeat(32))
+    const wallet = Wallet.createRandom()
+
+    const feed = bee.makeFeedWriter('sequence', '00'.repeat(32), wallet.privateKey)
+    const rootChunk = await bee.createFeedManifest(feedStamp, 'sequence', '00'.repeat(32), wallet.address)
+    console.log(rootChunk.reference)
 
     System.forever(async () => {
         const files = await readdir('./media/live')
@@ -20,19 +26,25 @@ export async function startUploader() {
             if (processedSegments[file]) {
                 continue
             }
+            console.log('Interested in file =>', file)
             const path = `./media/live/${file}`
             const data = await readFile(path)
-            const uploadedSegment = await bee.uploadData(stamp, data, { deferred: true })
+            console.log('Data has length', data.length)
+            const uploadedSegment = await bee.uploadFile(stamp, data, 'segment.ts', {
+                contentType: 'video/mp2t',
+                deferred: true
+            })
             processedSegments[file] = uploadedSegment.reference
             await unlink(path)
             const playlist = createPlaylist(Object.values(processedSegments))
             const uploadedPlaylist = await bee.uploadFile(stamp, playlist, 'index.m3u8', {
-                contentType: 'application/vnd.apple.mpegurl'
+                contentType: 'application/x-mpegURL',
+                deferred: true
             })
-            const feedUploadResults = await feed.upload(stamp, uploadedPlaylist.reference)
-            console.log('Uploaded segment =>', uploadedSegment.reference)
-            console.log('Uploaded playlist =>', uploadedPlaylist.reference)
-            console.log('Uploaded feed =>', feedUploadResults)
+            const feedUploadResults = await feed.upload(feedStamp, uploadedPlaylist.reference, { deferred: true })
+            console.log('Uploaded segment =>', `http://localhost:1633/bzz/${uploadedSegment.reference}/`)
+            console.log('Uploaded playlist =>', `http://localhost:1633/bzz/${uploadedPlaylist.reference}/`)
+            console.log('Uploaded feed =>', `http://localhost:1633/bzz/${feedUploadResults}/`)
         }
     }, 1000)
 }
